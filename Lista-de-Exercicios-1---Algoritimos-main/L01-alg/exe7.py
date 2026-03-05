782d4dc239ee36ce84d6bdb29c9515231060f19b
Carlos <carloseduardoperetiatko07@gmail.com>
2026-03-05 00:18:22 -0300

    08

---
+++ Lista-de-Exercicios-1---Algoritimos-main/L01-alg/exe7.py
@@ -0,0 +1,5 @@
+# Soma dos n primeiros números positivos. Escreva um programa Python que receba do
+# usuário um número inteiro positivo n e então exiba a soma de todos os números inteiros de 1 a
+# n. Tal soma pode ser computada usando a seguinte fórmula:
+#(n)(n+1)/2
+
